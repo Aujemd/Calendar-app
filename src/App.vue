d732471c3279f66e0c4cc7ef0489b8a2537faac1
Aujemd <johanstriker05@gmail.com>
2019-03-02 17:33:14 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <b-navbar toggleable ="md" type="dark" variant ="danger" >
+      <b-container>
+        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+        <b-navbar-toggle target="nav_collapse" />
+
+        <b-collapse is-nav id="nav_collapse">
+
+            <b-navbar-nav>
+              <b-nav-item :to="{name: 'home'}">Home</b-nav-item>
+              <b-nav-item :to="{name: 'about'}">About</b-nav-item>
+            </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+  </div>
+</template>
+
+
+
+
